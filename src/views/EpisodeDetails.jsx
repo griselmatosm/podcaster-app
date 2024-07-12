dcf24889d459de3893fd18099d40f5641b35ae57
griselmatosm <grisel@marketgoo.com>
2024-07-13 00:53:26 +0200

    feat: Episode Detail and css variables

--- src/views/EpisodeDetails.jsx
+++ src/views/EpisodeDetails.jsx
@@ -1,15 +1,12 @@
 import { useParams, useOutletContext } from "react-router-dom";
+import { EpisodePanel } from "../components/EpisodePanel/EpisodePanel";
 export const EpisodeDetails = () => {
   const { episodeId } = useParams();
   const episodes = useOutletContext();
   const episode = episodes.find(
     (episode) => JSON.stringify(episode.id) === episodeId
   );
-  return (
-    <div>
-      <h1>Episode Details</h1>
-      <h2>{episode.title}</h2>
-      <p>{episode.description}</p>
-    </div>
-  );
+
+  if (!episode) return <div>Episode not found</div>;
+  return <EpisodePanel episode={episode} />;
 };
